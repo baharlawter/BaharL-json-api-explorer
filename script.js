e5f3030888946d82fc55952db6341e957a53cd5b
bahar.lawter@gmail.com <baharlawter@gmail.com>
2025-06-02 09:14:56 -0500

    working fetch button

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// so much empty
+document.getElementById("fetchButton").addEventListener("click", fetchPosts);
+
+function fetchPosts() {
+  const postList = document.getElementById("postList");
+  const errorDiv = document.getElementById("error");
+  postList.innerHTML = "<p>Posts are loading</p>";
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((posts) => {
+      console.log(posts);
+      postList.innerHTML = "";
+      posts.forEach(() => {
+        document.createElement("div");
+        constElement.add("post");
+        postList.appendChild(postElement);
+      });
+    })
+    .catch((error) => {
+      errorDiv.textContent = `An error occurred: ${error}`;
+      postList.innerHTML = "";
+    });
+}
