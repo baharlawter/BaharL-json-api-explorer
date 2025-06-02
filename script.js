e7d4ba315f5545e9544f5693e1357e829107a67b
bahar.lawter@gmail.com <baharlawter@gmail.com>
2025-06-02 09:27:52 -0500

    fetchig data is working now

--- script.js
+++ script.js
@@ -4,19 +4,22 @@ function fetchPosts() {
   const postList = document.getElementById("postList");
   const errorDiv = document.getElementById("error");
   postList.innerHTML = "<p>Posts are loading</p>";
+
   fetch("https://jsonplaceholder.typicode.com/posts")
     .then((response) => response.json())
     .then((posts) => {
       console.log(posts);
       postList.innerHTML = "";
-      posts.forEach(() => {
-        document.createElement("div");
-        constElement.add("post");
+
+      posts.forEach((post) => {
+        const postElement = document.createElement("div");
+        postElement.classList.add("post");
+        postElement.innerHTML = `<h3>${post.title}</h3><p>${post.body}</p>`;
         postList.appendChild(postElement);
       });
     })
     .catch((error) => {
-      errorDiv.textContent = `An error occurred: ${error}`;
+      errorDiv.textContent = `An error occurred`;
       postList.innerHTML = "";
     });
 }
